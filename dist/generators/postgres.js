0923f7f761e0c458331611e52348d76d7a9e10ba
Zac McCormick <zac.mccormick@gmail.com>
2016-06-17 14:32:14 -0400

    Use FCM_ConvertToDouble instead

--- dist/generators/postgres.js
+++ dist/generators/postgres.js
@@ -25,18 +25,21 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 // This function is required in the database
 
 /*
-CREATE OR REPLACE FUNCTION convert_to_float(input_value text)
-RETURNS FLOAT AS $$
-DECLARE float_value FLOAT DEFAULT NULL;
+DROP FUNCTION IF EXISTS FCM_ConvertToFloat(input_value text);
+CREATE OR REPLACE FUNCTION FCM_ConvertToFloat(input_value text)
+  RETURNS double precision AS
+$BODY$
+DECLARE float_value double precision DEFAULT NULL;
 BEGIN
   BEGIN
-    float_value := input_value::float;
+    float_value := input_value::double precision;
   EXCEPTION WHEN OTHERS THEN
     RETURN NULL;
   END;
 RETURN float_value;
 END;
-$$ LANGUAGE plpgsql;
+$BODY$
+LANGUAGE 'plpgsql' IMMUTABLE STRICT;
 */
 
 var TYPES = {
@@ -80,7 +83,7 @@ var Postgres = (function (_SchemaGenerator) {
   }, {
     key: 'transformToDouble',
     value: function transformToDouble(columnName) {
-      return (0, _util.format)('convert_to_float(%s)', columnName);
+      return (0, _util.format)('FCM_ConvertToFloat(%s)', columnName);
     }
   }, {
     key: 'primaryKeyName',
--- src/generators/postgres.js
+++ src/generators/postgres.js
@@ -4,18 +4,21 @@ import {format as fmt} from 'util';
 // This function is required in the database
 
 /*
-CREATE OR REPLACE FUNCTION convert_to_float(input_value text)
-RETURNS FLOAT AS $$
-DECLARE float_value FLOAT DEFAULT NULL;
+DROP FUNCTION IF EXISTS FCM_ConvertToFloat(input_value text);
+CREATE OR REPLACE FUNCTION FCM_ConvertToFloat(input_value text)
+  RETURNS double precision AS
+$BODY$
+DECLARE float_value double precision DEFAULT NULL;
 BEGIN
   BEGIN
-    float_value := input_value::float;
+    float_value := input_value::double precision;
   EXCEPTION WHEN OTHERS THEN
     RETURN NULL;
   END;
 RETURN float_value;
 END;
-$$ LANGUAGE plpgsql;
+$BODY$
+LANGUAGE 'plpgsql' IMMUTABLE STRICT;
 */
 
 const TYPES = {
@@ -45,7 +48,7 @@ export default class Postgres extends SchemaGenerator {
   // alternate:
   // select '-1.2e10' ~ '^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$';
   transformToDouble(columnName) {
-    return fmt('convert_to_float(%s)', columnName);
+    return fmt('FCM_ConvertToFloat(%s)', columnName);
   }
 
   primaryKeyName(table) {
